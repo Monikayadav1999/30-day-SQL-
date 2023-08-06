40faba307bcdd5e4dd60e74fb15a7b8930066ad0
Monika Yadav <68689152+Monikayadav1999@users.noreply.github.com>
2023-08-06 13:14:59 +0530

    Create Combine Two Tables.sql

---
+++ Combine Two Tables.sql
@@ -0,0 +1,11 @@
+Question Link: https://leetcode.com/problems/combine-two-tables/?envType=list&envId=e97a9e5m
+
+SELECT 
+      P.lastName, 
+      P.firstName, 
+      A.city, 
+      A.state
+FROM 
+      Person P LEFT JOIN Address A 
+      ON
+      P.personID = A.personID;
