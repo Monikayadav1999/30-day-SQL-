41ed0192ef5bbb1aeef2db88a7c0670843dd4c16
Monika Yadav <68689152+Monikayadav1999@users.noreply.github.com>
2023-08-06 22:56:16 +0530

    Create Second Highest Salary.sql

---
+++ Second Highest Salary.sql
@@ -0,0 +1,10 @@
+Question : https://leetcode.com/problems/nth-highest-salary/?envType=list&envId=e97a9e5m
+Level : Medium
+  
+SELECT ( 
+        SELECT NTH_VALUE(Salary, 2) OVER(ORDER BY Salary DESC) AS SecondHighestSalary
+        FROM Employee
+        GROUP BY Salary
+        LIMIT 1 OFFSET 1 ) AS SecondHighestSalary;
+
+# NTH_VALUE() is Window Ranking Function 
