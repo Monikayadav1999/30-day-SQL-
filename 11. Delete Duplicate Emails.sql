c01d707a9bcad39b749548fc0187d3a1b19a796f
Monika Yadav <68689152+Monikayadav1999@users.noreply.github.com>
2023-08-10 14:09:45 +0530

    Update 11. Delete Duplicate Emails.sql

--- 11. Delete Duplicate Emails.sql
+++ 11. Delete Duplicate Emails.sql
@@ -10,3 +10,13 @@ WHERE
       P1.email = P2.email
       AND 
       P1.id > P2.id;
+
+# Using Join 
+
+DELETE 
+      P1
+FROM 
+      person P1 JOIN person P2 
+      ON 
+      P1.id > P2.id AND P1.email = P2.email;
+
