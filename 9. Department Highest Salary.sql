a7c94cec48e5962ea7a2c6cacf75f94ffae67409
Monika Yadav <68689152+Monikayadav1999@users.noreply.github.com>
2023-08-10 13:35:02 +0530

    Create 9. Department Highest Salary.sql

---
+++ 9. Department Highest Salary.sql
@@ -0,0 +1,39 @@
+Question : https://leetcode.com/problems/department-highest-salary/?envType=list&envId=e97a9e5m
+Level : Medium
+
+# Using CTE 
+  
+WITH RANKING AS 
+(
+  SELECT
+            e.name, e.salary, e.departmentId,
+            d.id, d.name AS Department,
+            DENSE_RANK() OVER(PARTITION BY d.id ORDER BY salary DESC) AS ranking
+       FROM 
+            Employee e JOIN department d
+            ON e.departmentId = d.id
+) 
+SELECT
+       Department, 
+       name AS Employee , 
+       salary AS Salary 
+FROM Ranking R1
+WHERE ranking = 1;
+
+# Without Using CTE
+
+SELECT
+       Department, 
+       name AS Employee , 
+       salary AS Salary 
+FROM (
+       SELECT
+            e.name, e.salary, e.departmentId,
+            d.id, d.name AS Department,
+            DENSE_RANK() OVER(PARTITION BY d.id ORDER BY salary DESC) AS ranking
+       FROM 
+            Employee e JOIN department d
+            ON e.departmentId = d.id
+      ) AS temp
+WHERE ranking = 1;
+
